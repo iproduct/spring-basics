faade6fdace94317ef720b7f13a1c66baed99bc9
Trayan Iliev <office@iproduct.org>
2020-05-26 17:58:55 +0300

    servlet-cookie-session demo initial commit

--- servlet-cookie-session/src/main/java/demo/spring/cookiesession/servlet/ShoppingServlet.java
+++ servlet-cookie-session/src/main/java/demo/spring/cookiesession/servlet/ShoppingServlet.java
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.package demo.spring.cookiesession.servlet;
+package demo.spring.cookiesession.servlet;
 
 import demo.spring.cookiesession.dao.BookDBController;
 import demo.spring.cookiesession.exception.NonexistingEntityException;
